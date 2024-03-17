2245955ab0124f44f675e6f67d2b826cd8b51c11
unknown <nachiket919156@gmail.com>
2024-03-17 22:52:31 +0530

    first

--- src/main/java/com/driver/OrderController.java
+++ src/main/java/com/driver/OrderController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
     @Autowired
-    private OrderService orderService;
+    private OrderService orderService = new OrderService();
 
 
     @PostMapping("/add-order")
